a87ba543624e1b5fa78281f244b629ea8d91451f
robert.xu <robert.xu@ndpmedia.com>
2015-03-31 17:21:33 +0800

    fix : use highcharts show consume progress.

--- rocketmq-cockpit/cockpit-common/src/main/java/com/ndpmedia/rocketmq/cockpit/mybatis/mapper/ConsumeProgressMapper.java
+++ rocketmq-cockpit/cockpit-common/src/main/java/com/ndpmedia/rocketmq/cockpit/mybatis/mapper/ConsumeProgressMapper.java
@@ -6,28 +6,50 @@ import org.apache.ibatis.annotations.Param;
 import java.util.Date;
 import java.util.List;
 
-public interface ConsumeProgressMapper {
+public interface ConsumeProgressMapper
+{
 
     long insert(ConsumeProgress consumeProgress);
 
     void delete(long id);
 
+    List<String> consumerGroupList();
+
+    List<String> topicList(@Param("consumerGroup") String consumerGroup);
+
+    List<String> brokerList(@Param("consumerGroup") String consumerGroup, @Param("topic") String topic);
+
+    List<String> queueList(@Param("consumerGroup") String consumerGroup, @Param("topic") String topic,
+            @Param("brokerName") String brokerName);
+
+
     /**
      * Retrieve consume progress records by specified parameters. All of these parameters are optional.
+     *
      * @param consumerGroup Optional consumer group.
-     * @param topic Optional topic.
-     * @param brokerName Optional broker name.
-     * @param queueId Optional queue ID, use -1 in case all queues are wanted.
+     * @param topic         Optional topic.
+     * @param brokerName    Optional broker name.
+     * @param queueId       Optional queue ID, use -1 in case all queues are wanted.
      * @return list of consume progress records.
      */
-    List<ConsumeProgress> list(@Param("consumerGroup") String consumerGroup,
-                               @Param("topic")String topic,
-                               @Param("brokerName")String brokerName,
-                               @Param("queueId")int queueId);
+    List<ConsumeProgress> diffList(@Param("consumerGroup") String consumerGroup, @Param("topic") String topic,
+            @Param("brokerName") String brokerName, @Param("queueId") int queueId);
 
+    /**
+     * Retrieve consume progress records by specified parameters. All of these parameters are optional.
+     *
+     * @param consumerGroup Optional consumer group.
+     * @param topic         Optional topic.
+     * @param brokerName    Optional broker name.
+     * @param queueId       Optional queue ID, use -1 in case all queues are wanted.
+     * @return list of consume progress records.
+     */
+    List<ConsumeProgress> list(@Param("consumerGroup") String consumerGroup, @Param("topic") String topic,
+            @Param("brokerName") String brokerName, @Param("queueId") int queueId);
 
     /**
      * Delete data that are older than the specified date.
+     *
      * @param date specified date
      * @return number of rows deleted.
      */
