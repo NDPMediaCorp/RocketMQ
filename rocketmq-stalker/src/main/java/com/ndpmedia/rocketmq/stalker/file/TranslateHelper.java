6cc1b936c95f51b551986856131772487b6a5a31
masterasia <41795353@qq.com>
2015-04-15 17:40:04 +0800

    fix:log more message.

--- rocketmq-stalker/src/main/java/com/ndpmedia/rocketmq/stalker/file/TranslateHelper.java
+++ rocketmq-stalker/src/main/java/com/ndpmedia/rocketmq/stalker/file/TranslateHelper.java
@@ -11,27 +11,26 @@ import java.util.Map.Entry;
  * Created by robert.xu on 2015/4/10.
  */
 public class TranslateHelper {
-    public static Map<String, Object> translateStringToMap(String line){
+    public static Map<String, Object> translateStringToMap(String line) {
         String base = line.substring(
                 line.indexOf(DataConfig.getProperties().getProperty("log.type")) + (DataConfig.getProperties()
-                                                                                              .getProperty("log.type"))
+                        .getProperty("log.type"))
                         .length());
-
 
         Map<String, Object> map = new HashMap<String, Object>();
         try {
             map = (Map<String, Object>) JSON.parse(base);
-        }catch (Exception e){
-            System.out.println(" this log is not format .");
+        } catch (Exception e) {
+            System.out.println(" this log is not format ." + line);
         }
         return map;
     }
 
-    public static String translateSQLFromMap(Map<String, Object> map){
+    public static String translateSQLFromMap(Map<String, Object> map) {
         String sql = DataConfig.getProperties().getProperty(map.get("sql").toString());
-        Set<Entry<String, Object>> set =map.entrySet();
+        Set<Entry<String, Object>> set = map.entrySet();
 
-        for (Entry<String, Object> entry:set){
+        for (Entry<String, Object> entry : set) {
             sql = sql.replaceAll("#" + entry.getKey(), entry.getValue() + "");
         }
 
