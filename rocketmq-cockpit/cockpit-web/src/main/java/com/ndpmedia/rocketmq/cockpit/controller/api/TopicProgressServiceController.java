07f26c650e3cba950c759aba9028a51e6bcf4c76
masterasia <41795353@qq.com>
2015-04-16 16:02:51 +0800

    fix:could find the topic who never had consumer group

--- rocketmq-cockpit/cockpit-web/src/main/java/com/ndpmedia/rocketmq/cockpit/controller/api/TopicProgressServiceController.java
+++ rocketmq-cockpit/cockpit-web/src/main/java/com/ndpmedia/rocketmq/cockpit/controller/api/TopicProgressServiceController.java
@@ -1,15 +1,21 @@
 package com.ndpmedia.rocketmq.cockpit.controller.api;
 
-import com.ndpmedia.rocketmq.cockpit.model.*;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.remoting.exception.RemotingException;
+import com.alibaba.rocketmq.tools.admin.DefaultMQAdminExt;
+import com.ndpmedia.rocketmq.cockpit.model.ConsumeProgress;
 import com.ndpmedia.rocketmq.cockpit.mybatis.mapper.ConsumeProgressMapper;
-import com.ndpmedia.rocketmq.cockpit.util.LoginConstant;
-import com.ndpmedia.rocketmq.cockpit.util.WebHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping(value = "/api/topic-progress")
@@ -26,7 +32,31 @@ public class TopicProgressServiceController {
 
     @RequestMapping(value = "/{topic}", method = RequestMethod.GET)
     @ResponseBody
-    public List<ConsumeProgress> list(@PathVariable("topic") String topic){
+    public List<ConsumeProgress> list(@PathVariable("topic") String topic) {
         return consumeProgressMapper.brokerTPSList(null, topic, null, -1);
+    }
+
+    @RequestMapping(value = "/d", method = RequestMethod.GET)
+    @ResponseBody
+    public List<String> simple() {
+        List<String> result = new ArrayList<String>();
+        DefaultMQAdminExt defaultMQAdminExt = new DefaultMQAdminExt();
+        try {
+            defaultMQAdminExt.start();
+            Set<String> bT = defaultMQAdminExt.fetchAllTopicList().getTopicList();
+            List<String> dT = consumeProgressMapper.topicList(null);
+            for (String temp : bT) {
+                if (!dT.contains(temp))
+                    result.add(temp);
+            }
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (RemotingException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
