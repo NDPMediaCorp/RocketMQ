c3b48c82e98ec2cd5d7406ba789d6db4fb62e6e9
masterasia <41795353@qq.com>
2015-04-20 17:46:47 +0800

    fix:little update.

--- develop.sh
+++ develop.sh
@@ -11,4 +11,4 @@ fi
 export PATH=/opt/taobao/mvn/bin:$JAVA_HOME/bin:$PATH
 mvn -Dmaven.test.skip=true clean package install assembly:assembly -U
 
-ln -s target/alibaba-rocketmq-3.2.0-SNAPSHOT.dir/alibaba-rocketmq devenv
+ln -s target/alibaba-rocketmq-3.2.2/alibaba-rocketmq devenv
