55584d18ba8351a5c62cf1b4ec4982c459ccb64d
Li Zhanhui <lizhanhui@gmail.com>
2015-08-28 17:32:32 +0800

    upgrade version

--- install.sh
+++ install.sh
@@ -19,5 +19,5 @@ export PATH=${M2_HOME}/bin:$JAVA_HOME/bin:$PATH
 set MAVEN_OPTS=-Xms512M -Xmx512M -XX:MaxPermSize=256M -XX:PermSize=256M
 mvn -Dmaven.test.skip=true clean package install assembly:assembly -U
 
-ln -s target/alibaba-rocketmq-3.2.2.R2/alibaba-rocketmq devenv
+ln -s target/alibaba-rocketmq-3.2.2.R2-SNAPSHOT/alibaba-rocketmq devenv
 cp ${JAVA_HOME}/jre/lib/ext/sunjce_provider.jar devenv/lib/
