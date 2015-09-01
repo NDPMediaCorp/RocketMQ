eb1fe7a8fc83206afbd53176faf185e65db7b1f5
masterasia <41795353@qq.com>
2015-09-01 16:18:18 +0800

    feature: multi stash and pop

--- rocketmq-client/src/test/java/com/alibaba/rocketmq/client/producer/concurrent/DefaultLocalMessageStoreTest.java
+++ rocketmq-client/src/test/java/com/alibaba/rocketmq/client/producer/concurrent/DefaultLocalMessageStoreTest.java
@@ -8,6 +8,9 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class DefaultLocalMessageStoreTest {
 
@@ -50,6 +53,65 @@ public class DefaultLocalMessageStoreTest {
         }
         cleanUp();
     }
+
+    @Test
+    public void testStashAndPopMulti() throws InterruptedException, IOException {
+        final int totalMessageCount = Integer.parseInt(System.getProperty("messagecounts", "1000"));
+        final int poolSize = Integer.parseInt(System.getProperty("poolsize", "10"));
+        final String topic = "Topic";
+        final byte[] body = "Data".getBytes();
+        final String key = "abc";
+        final String value = "value";
+        final AtomicInteger ai = new AtomicInteger(0);
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(poolSize);
+        for (int i = 0; i < poolSize; i++) {
+            scheduledExecutorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < totalMessageCount; i++) {
+                        Message message = new Message(topic, body);
+                        message.setKey(key);
+                        message.putUserProperty(key, value);
+                        defaultLocalMessageStore.stash(message);
+                    }
+                    ai.addAndGet(1);
+//                    System.out.println(ai.addAndGet(1) + " this finish ");
+
+                }
+            });
+        }
+
+        while (true) {
+            if (ai.get() == poolSize){
+                break;
+            }
+
+            Thread.sleep(1000);
+//            System.out.println(" just wait ");
+        }
+
+
+        defaultLocalMessageStore.close();
+
+//        System.out.println("finish insert ");
+
+        defaultLocalMessageStore = new DefaultLocalMessageStore(STORE_NAME);
+        int poppedOut = 0;
+        Message[] messages = defaultLocalMessageStore.pop(20);
+        while (null != messages && messages.length > 0) {
+            poppedOut += messages.length;
+            for (Message msg : messages) {
+                Assert.assertEquals(topic, msg.getTopic());
+                Assert.assertArrayEquals(body, msg.getBody());
+                Assert.assertEquals(key, msg.getKeys());
+                Assert.assertEquals(value, msg.getProperty(key));
+            }
+            messages = defaultLocalMessageStore.pop(2);
+        }
+        Assert.assertEquals(totalMessageCount * poolSize, poppedOut);
+        defaultLocalMessageStore.close();
+    }
+
 
     @Test
     public void testStashAndPop() throws InterruptedException, IOException {
