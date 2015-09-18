2ff0e1fc041223b6c25217d1b2eabeefb8ee538b
Li Zhanhui <lizhanhui@gmail.com>
2015-09-18 15:12:05 +0800

    Adjust parameter

--- rocketmq-store/src/main/java/com/alibaba/rocketmq/store/config/MessageStoreConfig.java
+++ rocketmq-store/src/main/java/com/alibaba/rocketmq/store/config/MessageStoreConfig.java
@@ -81,18 +81,23 @@ public class MessageStoreConfig {
     private int flushCommitLogThoroughInterval = 1000 * 10;
     // 刷ConsumeQueue，彻底刷盘间隔时间
     private int flushConsumeQueueThoroughInterval = 1000 * 60;
+
     // 最大被拉取的消息字节数，消息在内存
     @ImportantField
-    private int maxTransferBytesOnMessageInMemory = 1024 * 256;
+    private int maxTransferBytesOnMessageInMemory = 1024 * 512;
+
     // 最大被拉取的消息个数，消息在内存
     @ImportantField
-    private int maxTransferCountOnMessageInMemory = 32;
+    private int maxTransferCountOnMessageInMemory = 128;
+
     // 最大被拉取的消息字节数，消息在磁盘
     @ImportantField
-    private int maxTransferBytesOnMessageInDisk = 1024 * 64;
+    private int maxTransferBytesOnMessageInDisk = 1024 * 128;
+
     // 最大被拉取的消息个数，消息在磁盘
     @ImportantField
-    private int maxTransferCountOnMessageInDisk = 8;
+    private int maxTransferCountOnMessageInDisk = 32;
+
     // 命中消息在内存的最大比例
     @ImportantField
     private int accessMessageInMemoryMaxRatio = 40;
